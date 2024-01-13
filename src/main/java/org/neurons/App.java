da91fbd3a5950c66a3bc5796b80b6cafa8e48b62
Wojciech Kesek <wojtekesek@gmail.com>
2024-01-13 09:20:33 +0100

    activation function

--- src/main/java/org/neurons/App.java
+++ src/main/java/org/neurons/App.java
@@ -8,6 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        double[] input = {0,1};
+        double[] weight = {0.5, 0.5};
+        double bias = 0.5;
+        double sum = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            sum += input[i] * weight[i];
+        }
+
+        sum += bias;
+
+        double activation = sum > 0 ? 1 : 0;
+        System.out.println(activation);
     }
 }
